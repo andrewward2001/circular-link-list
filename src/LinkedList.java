81f26e3c1545cc06bddcd33cd0d7f879badc0f3e
andrewsayegh <andrew_sayegh@student.wayland.k12.ma.us>
2017-10-17 14:19:40 -0400

    edits

---
+++ src/LinkedList.java
@@ -0,0 +1,279 @@
+/**
+ * Created by student on 10/17/17.
+ */
+
+import java.util.*;
+
+public class LinkedList<T> implements Iterable<T>
+{
+    private Node<T> root;
+
+    /** Constructs an empty list **/
+
+    public LinkedList() { root = null; }
+
+    /** Returns true if the list is empty */
+
+    public boolean isEmpty() { return root == null; }
+
+    /** Inserts a new node at the beginning of this list. */
+
+    public void addFirst(T item) { root = new Node<T>(item, root); }
+
+    /** Returns the first element in the list. */
+
+    public T getFirst()
+    {
+        if(root == null) throw new NoSuchElementException();
+
+        return root.data;
+    }
+
+    /** Removes the first element in the list. */
+
+    public T removeFirst()
+    {
+        T tmp = getFirst();
+        root = root.next;
+        return tmp;
+    }
+    /** Inserts a new node to the end of this list. */
+
+    public void addLast(T item)
+    {
+        if( root == null)
+            addFirst(item);
+        else
+        {
+            Node<T> temp = root;
+            while(temp.next != null) temp = temp.next;
+
+            temp.next = new Node<T>(item, null);
+        }
+    }
+
+    /** Returns the last element in the list. */
+
+    public T getLast()
+    {
+        if(root == null) throw new NoSuchElementException();
+
+        Node<T> tmp = root;
+        while(tmp.next != null) tmp = tmp.next;
+
+        return tmp.data;
+    }
+
+    /** Removes all nodes from the list. */
+
+    public void clear() { root = null; }
+
+    /** Returns true if this list contains the specified element. */
+    public boolean contains(T x)
+    {
+        for(T tmp : this)
+            if(tmp.equals(x)) return true;
+
+        return false;
+    }
+
+    /** Returns the data at the specified position in the list. */
+
+    public T get(int pos)
+    {
+        if (root == null) throw new IndexOutOfBoundsException();
+
+        Node<T> tmp = root;
+        for (int k = 0; k < pos; k++) tmp = tmp.next;
+
+        if( tmp == null) throw new IndexOutOfBoundsException();
+
+        return tmp.data;
+    }
+
+    /**  Returns a string representation */
+
+    public String toString()
+    {
+        StringBuffer result = new StringBuffer();
+        for(Object x : this)
+            result.append(x + " ");
+
+        return result.toString();
+    }
+
+    /** Inserts a new node after a node containing the key. */
+
+    public void insertAfter(T key, T toInsert)
+    {
+        Node<T> tmp = root;
+
+        while(tmp != null && !tmp.data.equals(key)) tmp = tmp.next;
+
+        if(tmp != null)
+            tmp.next = new Node<T>(toInsert, tmp.next);
+    }
+
+    /** Inserts a new node before a node containing the key. */
+
+    public void insertBefore(T key, T toInsert)
+    {
+        if(root == null) return;
+
+        if(root.data.equals(key))
+        {
+            addFirst(toInsert);
+            return;
+        }
+
+        Node<T> previous = null;
+        Node<T> current = root;
+
+        while(current != null && !current.data.equals(key))
+        {
+            previous = current;
+            current = current.next;
+        }
+        //insert between cur and prev
+        if(current != null)
+            previous.next = new Node<T>(toInsert, current);
+    }
+
+    /** Removes the first occurrence of the specified element in this list. */
+
+    public void remove(T key)
+    {
+        if(root == null)
+            throw new RuntimeException("ERROR -> Couldn't delete");
+
+        if( root.data.equals(key) )
+        {
+            root = root.next;
+            return;
+        }
+
+        Node<T> current  = root;
+        Node<T> previous = null;
+
+        while(current != null && !current.data.equals(key) )
+        {
+            previous = current;
+            current = current.next;
+        }
+
+        if(current == null)
+            throw new RuntimeException("ERROR -> Couldn't delete");
+
+        /** delete cur node */
+
+        previous.next = current.next;
+    }
+
+    /** Returns a copy of the list
+     *  Complexity: O(n^2)
+     */
+
+
+    public  LinkedList<T> copy1()
+    {
+        LinkedList<T> twin = new LinkedList<T>();
+        Node<T> temp = root;
+        while(temp != null)
+        {
+            twin.addLast( temp.data );
+            temp = temp.next;
+        }
+
+        return twin;
+    }
+
+    /** Returns a copy of the list
+     *  Complexity: O(n)
+     */
+
+    public LinkedList<T> copy2()
+    {
+        LinkedList<T> twin = new LinkedList<T>();
+        Node<T> temp = root;
+        while(temp != null)
+        {
+            twin.addFirst( temp.data );
+            temp = temp.next;
+        }
+
+        return twin.reverse();
+    }
+
+    /** Reverses the list
+     * Complexity: O(n)
+     */
+
+    public LinkedList<T> reverse()
+    {
+        LinkedList<T> list = new LinkedList<T>();
+        Node<T> temp = root;
+        while(temp != null)
+        {
+            list.addFirst( temp.data );
+            temp = temp.next;
+        }
+        return list;
+    }
+
+    /**
+     *  Returns a copy of the immutable list
+     *  It uses a tail reference.
+     *  Complexity: O(n)
+     */
+
+    public LinkedList<T> copy3()
+    {
+        LinkedList<T> twin = new LinkedList<T>();
+        Node<T> temp = root;
+        if(root==null) return null;
+        twin.root = new Node<T>(root.data, null);
+        Node<T> tmpTwin = twin.root;
+        while(temp.next != null)
+        {
+            temp = temp.next;
+            tmpTwin.next = new Node<T>(temp.data, null);
+            tmpTwin = tmpTwin.next;
+        }
+
+        return twin;
+    }
+
+    /*******************************************************
+     *
+     *  Iterator
+     *
+     ********************************************************/
+
+    public Iterator<T> iterator() { return new LinkedListIterator(); }
+
+    private class LinkedListIterator  implements Iterator<T>
+    {
+        private Node<T> nextNode;
+
+        public LinkedListIterator() { nextNode = root; }
+
+        public boolean hasNext() { return nextNode != null; }
+
+        public T next()
+        {
+            if (!hasNext()) throw new NoSuchElementException();
+            T res = nextNode.data;
+            nextNode = nextNode.next;
+            return res;
+        }
+
+        public void remove() { throw new UnsupportedOperationException(); }
+    }
+
+    /** Include Main */
+
+    public static void main(String[] args)
+    {
+        LinkedList<String> list = new LinkedList<String>();
+    }
+}
--- src/Node.java
+++ src/Node.java
@@ -2,34 +2,14 @@
  * Created by student on 10/17/17.
  */
 
-import java.util.*;
-import java.util.function.Consumer;
+public class Node<T>
+{
+    public T data;
+    public Node<T> next;
 
-public class Node<T> implements Iterable<T> {
-    private Node<T> root;
-    public Node(){
-        root = null;
-    }
-
-    public boolean isEmpty()
+    public Node(T data, Node<T> next)
     {
-        return root == null;
-    }
-
-    public void addFirst(T item)
-    {
-        root = new Node<T>(item, root);
-    }
-
-    public Iterator<T> iterator() {
-        return null;
-    }
-
-    public void forEach(Consumer<? super T> action) {
-
-    }
-
-    public Spliterator<T> spliterator() {
-        return null;
+        this.data = data;
+        this.next = next;
     }
 }
