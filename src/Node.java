fed6ae80f2a1a2f89fe1edad7a9167b1b2e7d2bd
andrewsayegh <andrew_sayegh@student.wayland.k12.ma.us>
2017-10-17 10:23:00 -0400

    edits

--- src/Node.java
+++ src/Node.java
@@ -4,5 +4,10 @@
 public class Node<T extends Comparable<T>> {
 
     private Node<T> root;
+    private T data;
 
+    public Node(T data, Node<T> root){
+        this.data = data;
+        this.root = root;
+    }
 }
