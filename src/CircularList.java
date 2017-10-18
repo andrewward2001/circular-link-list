80f0cc96151304c2267f91839ca7c1c4b0e2bdcd
andreweggleston <andrew_eggleston@student.wayland.k12.ma.us>
2017-10-18 08:40:55 -0400

    Starting CircularList

---
+++ src/CircularList.java
@@ -0,0 +1,12 @@
+/**
+ * Created by student on 10/18/17.
+ */
+public class CircularList<T> {
+    Node<T> first;
+    Node<T> last;
+
+    public CircularList(){
+        first = null;
+        last = null;
+    }
+}
