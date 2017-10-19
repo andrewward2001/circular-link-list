225da3724faa860998edeca31652fa8f33f7d8d5
andrewsayegh <andrew_sayegh@student.wayland.k12.ma.us>
2017-10-19 07:47:38 -0400

    Testing in main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList<String> list = new LinkedList<>();
+        LinkedList<String> list = new LinkedList<String>();
+        list.addFirst("Jolteon");
+        list.addFirst("Dusknoir");
+        list.addFirst("Deoxys");
+        list.addFirst("Reuniclus");
+        list.addFirst("Chimecho");
+        list.addFirst("Poochyena");
+        System.out.println(list);
     }
 }
