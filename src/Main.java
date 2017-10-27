d6a322db17b2f61136a5f4d9a8d197ac155ed51b
andreweggleston <andrew_eggleston@student.wayland.k12.ma.us>
2017-10-27 10:11:58 -0400

    kanming why

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,26 @@ import java.io.*;
 import java.util.ArrayList;
 
 
-public class Main extends JPanel{
+public class Main {
 
     static int size = 800;
+    static LinkedList<String> linkedList = new LinkedList<>();
 
     public static void main(String[] args) {
+
 
         JFrame window = new JFrame("circular linked lists!");
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setBounds(0, 0, 800, 800); //(x, y, w, h)
-
-
-
-
-
-        Main panel = new Main();
+        Panel panel = new Panel();
         panel.setFocusable(true);
         panel.grabFocus();
         window.add(panel);
         window.setVisible(true);
 
-        JFrame textWindow = new JFrame("Write your script here...");
+        JFrame textWindow = new JFrame("Paste the names here...");
         textWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         textWindow.setBounds(0, 0, size, size);
-
-
 
 
         JTextArea textArea = new JTextArea("Names");
@@ -37,34 +32,28 @@ public class Main extends JPanel{
         JScrollPane areaScrollPane = new JScrollPane(textArea);
         areaScrollPane.setVerticalScrollBarPolicy(
                 JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        areaScrollPane.setPreferredSize(new Dimension(size, size-80));
+        areaScrollPane.setPreferredSize(new Dimension(size, size - 80));
 
 
-
-
-
-        ArrayList<String> fileStrings = new ArrayList<String>(); //
+        ArrayList<String> fileStrings = new ArrayList<>(); //
         JButton runScriptButton = new JButton("Run Script");
-        runScriptButton.setSize(100,50);
+        runScriptButton.setSize(100, 50);
         ArrayList<String> finalFileStrings = fileStrings;
 
         runScriptButton.addActionListener(e -> {
 
 
-
-        //convert the list to something
+            //convert the list to something
             String names = textArea.getText();
             System.out.println(textArea.getText());
 
             String[] namesList = names.split("\n");
             finalFileStrings.clear();
-            for (String s: namesList) {
+            for (String s : namesList) {
                 finalFileStrings.add(s);
             }
 
-            for (int i = 0; i < finalFileStrings.size(); i++) {
-                System.out.println(finalFileStrings.get(i));
-            }
+            finalFileStrings.forEach(System.out::println);
 
 
         });
@@ -83,31 +72,10 @@ public class Main extends JPanel{
         textWindow.add(runScriptButton);
 
 
-        LinkedList<Node> linkedList = new LinkedList<Node>();
         for (int i = 0; i < finalFileStrings.size(); i++) {
-            linkedList.addLast(new Node(finalFileStrings.get(i), linkedList.getRoot()));
+            linkedList.addLast(finalFileStrings.get(i));
         }
 
-
-//        LinkedList<String> list;
-////        ArrayList<String> fileStrings;
-//
-//        try {
-//            list = new LinkedList<>();
-//            fileStrings = readFile("res" + File.separator + "names.txt");
-//            fileStrings.forEach(list::addLast);
-//
-//            list.remove("One");
-//
-//            Node temp = list.getRoot();
-//            for (int i = 0; i < 15; i++) {
-//                System.out.println(temp.data);
-//                temp = temp.getNext();
-//            }
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }
 
     public static ArrayList<String> readFile(String fileName) throws IOException {
@@ -119,5 +87,9 @@ public class Main extends JPanel{
         }
         reader.close();
         return Alist;
+    }
+
+    public static LinkedList getLinkedList() {
+        return linkedList;
     }
 }
--- src/Panel.java
+++ src/Panel.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
  */
 class Panel extends JPanel {
 
-    private Timer timer;
     private LinkedList<String> linkedList = new LinkedList<>();
     private ArrayList<Circle> circleList = new ArrayList<>();
     private int theta;
@@ -31,8 +30,10 @@ class Panel extends JPanel {
 
         theta = 360 / linkedList.size();
 
-        populateCircleList();
-
+        if (Main.getLinkedList().size() > 0) {
+            System.out.println("Size " + Main.getLinkedList().size());
+            populateCircleList();
+        }
 
 
         addMouseListener(new MouseListener() {
@@ -72,11 +73,11 @@ class Panel extends JPanel {
 
             @Override
             public void keyPressed(KeyEvent e) {
-                if(e.getKeyCode() == KeyEvent.VK_B){
+                if (e.getKeyCode() == KeyEvent.VK_B) {
                     System.out.println("linked size " + linkedList.size());
                     System.out.println("circle size " + circleList.size());
                 }
-                if(e.getKeyCode() == KeyEvent.VK_C){
+                if (e.getKeyCode() == KeyEvent.VK_C) {
                     circleList.forEach(System.out::println);
                 }
             }
@@ -92,14 +93,17 @@ class Panel extends JPanel {
     public void paint(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
         super.paint(g2);
-
-        theta = 360 / linkedList.size();
+        linkedList = Main.getLinkedList();
+        if (linkedList.size() != 0)
+            theta = 360 / linkedList.size();
 
         bigRadius = (getWidth() - smallDiameter) / 2;
 
         for (int i = 1; i <= linkedList.size(); i++) {
-            populateCircleList();
-            circleList.get(i - 1).draw(g2);
+            if (Main.getLinkedList().size() > 0) {
+                populateCircleList();
+                circleList.get(i - 1).draw(g2);
+            }
 //            g2.setColor(new Color(0,0,0));
 //            g2.fillOval((int)(bigRadius - bigRadius*(Math.cos(Math.toRadians(theta*i)))), (int)(bigRadius - bigRadius*(Math.sin(Math.toRadians(theta*i)))), 70, 70);
 //            g2.setColor(new Color(255, 255, 255));
@@ -108,11 +112,13 @@ class Panel extends JPanel {
         repaint();
     }
 
-    private void populateCircleList(){
+    private void populateCircleList() {
+
         circleList.clear();
-        for (int i = 1; i <= linkedList.size(); i++)
+        for (int i = 1; i <= linkedList.size(); i++) {
             circleList.add(new Circle((int) (bigRadius - bigRadius * (Math.cos(Math.toRadians(theta * i)))), (int) (bigRadius - bigRadius * (Math.sin(Math.toRadians(theta * i)))), smallDiameter, linkedList.get(i - 1)));
 
+        }
     }
 
 }
